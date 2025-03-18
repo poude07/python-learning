d72360e60d9000641bac696acb2ad176569f475a
poude07 <121388734+poude07@users.noreply.github.com>
2025-03-18 20:46:19 +0545

    Day 1

---
+++ test.py
@@ -0,0 +1,35 @@
+#add here when a,b,c are 5,10,5 resp
+
+a = 5
+b = 10
+c = 5
+
+sum_result = a+b+c
+print("Add:", sum_result)
+
+# sub here when same as above
+
+sub_result = a-b-c
+print("sub:", sub_result)
+
+# multi  here when same as above
+
+mul_result = a*b*c
+print("mul:", mul_result)
+
+# div here when same as above
+
+div_result = b/a
+print("div:", div_result)
+
+# div with out 2.0 
+floor_div_result = b//c
+print("floor div:", floor_div_result)
+
+# Addition (+) → Adds two numbers.Subtraction (-) 
+# #Subtracts one number from another.
+# #Multiplication (*) → Multiplies two numbers.
+#Division (/) → Performs normal division and returns a float (decimal number).
+#Floor Division (//) → Divides two numbers and returns an integer (removes the decimal part).
+#Modulus (%) → Returns the remainder when one number is divided by another.
+#Exponentiation (**) → Raises one number to the power of another (e.g., 10 ** 5 means 10^5).
