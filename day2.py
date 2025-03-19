406e93274d8fefc6c2dbd97039cfaaff622a4669
poude07 <121388734+poude07@users.noreply.github.com>
2025-03-19 17:30:06 +0545

    day2

---
+++ day2.py
@@ -0,0 +1,11 @@
+
+#taking input from the user
+# input() TAKES useer input as a string
+# AND we int() to convert it into an integer
+
+num1 = int(input("Enter a number:"))
+num2 = int(input("Enter second number:"))
+num3 = int(input("Enter third number:"))
+
+sum = num1 + num2 - num3
+print("Result:", sum)
